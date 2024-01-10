15a539105da8338e6483299f2159675ac0e0787d
BungasZungas <caiquefloureiro@gmail.com>
2024-01-10 03:35:25 -0300

    add menu and buttons

---
+++ Assets/Script/Buttons/ButtonScaler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ButtonScaler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float finalScale = 1.2f;
+    public float scaleDuration = .1f;
+
+    private Vector3 defaultScale;
+
+    private Tween _currentTween;
+
+    private void Awake()
+    {
+        defaultScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("ENTER");
+        //transform.localScale = Vector3.one * 1.2f;
+        _currentTween = transform.DOScale(defaultScale * finalScale, scaleDuration);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("EXIT");
+        _currentTween.Kill();
+        transform.localScale = defaultScale;
+    }
+}
